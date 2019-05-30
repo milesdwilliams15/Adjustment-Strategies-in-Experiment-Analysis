01cf6daa933c5dda527291057568c32bcbb67cc8
milesdwilliams15 <milesdwilliams15@gmail.com>
2019-05-30 13:01:14 -0500

    more work

--- code for different adjustment strategies.R
+++ code for different adjustment strategies.R
@@ -9,12 +9,12 @@ library(lmtest)
 
 # Generate an outcome Y as a quadradic function of X plus stochastic noise:
 N = 100 # number of observations
-ATE = 2 # True treatment effect
+ATE = 5 # True treatment effect
 df = tibble(
   X = rnorm(n=N,mean=50,sd=10), # X variable
-  Yc = .5*X - .01*X^2 + rnorm(n=N,sd=2), # Y, untreated
+  Yc = .3*(X - mean(X))^2 + rnorm(n=N,sd=10), # Y, untreated
   Yt = ATE + Yc, # Value of Y if treated
-  Z = rbinom(n=N,size=1,prob=.25), # Random treatment assignment (25% prob of treatment)
+  Z = rbinom(n=N,size=1,prob=.5), # Random treatment assignment (25% prob of treatment)
   Y = Z*Yt + (1-Z)*Yc # Post treatment outcome
 )
 
@@ -27,7 +27,7 @@ fit2 = lm(Y ~ Z + X, df)
 coeftest(fit2,vcovHC(fit2,type="HC2")) # Nope!!
 
 # What if I use Lin's (2012a) technique?
-fit3 = lm(Y ~ Z + X + Z*I(X - mean(X)), df)
+fit3 = lm(Y ~ Z*I(X - mean(X)), df)
 coeftest(fit3,vcovHC(fit3,type="HC2")) # Nope!!!
 
 # If I control for X and X^2?
@@ -43,15 +43,6 @@ Ypred = predict(rf)
 Yresd = df$Y - Ypred
 fit5 = lm(Yresd ~ Z, df)
 coeftest(fit5,vcovHC(fit5,type="HC2")) # Closer than other methods, but not there yet.
-
-
-# Can I use a variable selection method?
-lbd = cv.glmnet(x=with(df,cbind(X,X^2,X^3,X^4,X^5)),y=df$Y)$lambda.min
-lr = glmnet(x=with(df,cbind(X,X^2,X^3,X^4,X^5)),y=df$Y,alpha=1,
-            lambda = lbd)
-coef(lr) # X^2 and X^3 make the cut...
-fit6 = lm(Y ~ Z + I(X^2) + I(X^3), df)
-coeftest(fit6,vcovHC(fit6,type="HC2")) # Wrong specification, but it worked!
 
 
 # What about a support vector machine (SVM)?
@@ -72,5 +63,8 @@ coeftest(fit8,vcovHC(fit8,type="HC2")) # Hey! Not Bad!
 
 # Rank based transformation?
 Yrank = rank(df$Y)
-fit9 = lm(Yrank ~ Z, df)
+rfit = npreg(Yrank ~ rank(X), df)
+Ypred = predict(rfit)
+Yresd = Yrank - Ypred
+fit9 = lm(Yresd ~ Z, df)
 coeftest(fit9,vcovHC(fit9,type="HC2"))
