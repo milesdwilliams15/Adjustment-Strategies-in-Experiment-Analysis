18f4eb980ba380b3f1a7480f14ef0598c10d1683
milesdwilliams15 <milesdwilliams15@gmail.com>
2019-07-18 14:34:25 -0500

    updates

--- code for different adjustment strategies.R
+++ code for different adjustment strategies.R
@@ -8,13 +8,13 @@ library(sandwich)
 library(lmtest)
 
 # Generate an outcome Y as a quadradic function of X plus stochastic noise:
-N = 100 # number of observations
+N = 1000 # number of observations
 ATE = 5 # True treatment effect
 df = tibble(
   X = rnorm(n=N,mean=50,sd=10), # X variable
-  Yc = .3*(X - mean(X))^2 + rnorm(n=N,sd=10), # Y, untreated
+  Yc = 1 + .3*(X - mean(X))^2 + rnorm(n=N,sd=10), # Y, untreated
   Yt = ATE + Yc, # Value of Y if treated
-  Z = rbinom(n=N,size=1,prob=.5), # Random treatment assignment (25% prob of treatment)
+  Z = rbinom(n=N,size=1,prob=pnorm(scale(X))), # non-random treatment assignment (25% prob of treatment)
   Y = Z*Yt + (1-Z)*Yc # Post treatment outcome
 )
 
@@ -32,16 +32,18 @@ coeftest(fit3,vcovHC(fit3,type="HC2")) # Nope!!!
 
 # If I control for X and X^2?
 fit4 = lm(log(Y) ~ Z + X + I(X^2), df)
-coeftest(fit4,vcovHC(fit4,type="HC2")) # Yes!!!!
+coeftest(fit4,vcovHC(fit4,type="HC2")) # Nope!!!!
 
-
-# But what do I do if I don't know how to properly adjust for X?
+# Let's take a different approach
 
 # Can I use a random forest to residualize Y?
 rf = randomForest(Y ~ X, df)
 Ypred = predict(rf)
 Yresd = df$Y - Ypred
-fit5 = lm(Yresd ~ Z, df)
+rf = randomForest(Z ~ X, df)
+Zpred = predict(rf)
+Zresd = df$Z - Zpred
+fit5 = lm(Yresd ~ Zresd)
 coeftest(fit5,vcovHC(fit5,type="HC2")) # Closer than other methods, but not there yet.
 
 
@@ -49,7 +51,10 @@ coeftest(fit5,vcovHC(fit5,type="HC2")) # Closer than other methods, but not ther
 sv = svm(Y ~ X, df)
 Ypred = predict(sv)
 Yresd = df$Y - Ypred
-fit7 = lm(Yresd ~ Z, df)
+sv = svm(Z ~ X, df)
+Zpred = predict(sv)
+Zresd = df$Z - Zpred
+fit7 = lm(Yresd ~ Zresd, df)
 coeftest(fit7,vcovHC(fit7,type="HC2"))
 
 
@@ -57,6 +62,9 @@ coeftest(fit7,vcovHC(fit7,type="HC2"))
 npfit = npreg(Y ~ X, df)
 Ypred = predict(npfit)
 Yresd = df$Y - Ypred
+npfit = npreg(Z ~ X, df)
+Zpred = predict(npfit)
+Zresd = df$Z - Zpred
 fit8 = lm(Yresd ~ Z, df)
 coeftest(fit8,vcovHC(fit8,type="HC2")) # Hey! Not Bad!
 
